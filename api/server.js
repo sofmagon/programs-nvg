94f9abb0fd0c8ed554915f101d9d15de4baeee96
SOFMAGON <ada_hack545@womenintech.mx>
2024-05-30 09:52:01 -0600

    fix: server settings

--- api/server.js
+++ api/server.js
@@ -4,18 +4,24 @@ const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 
+// Usar middlewares predeterminados
 server.use(middlewares);
-// Antes de server.use(router)
+
+// Reescribir rutas antes de usar el router
 server.use(
-	jsonServer.rewriter({
-		"/api/programs": "/programs",
-		"/api/programs/:id": "/programs/:id"
-	})
+  jsonServer.rewriter({
+    "/api/programs": "/programs",
+    "/api/programs/:id": "/programs/:id"
+  })
 );
 
+// Usar el router
 server.use(router);
-server.listen(3000, () => {
-	console.log("El servidor JSON está funcionando");
+
+// Usar el puerto proporcionado por el entorno (como Vercel) o 3000 por defecto
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`El servidor JSON está funcionando en el puerto ${port}`);
 });
 
 // Exporta la API del Servidor
